88672823442b83afabb89a32bd63a6ce801fce09
Caitlin Wall <caitlinwall96@gmail.com>
2021-09-24 20:21:19 -0400

    Set up textareas so that entries can be made to the planner

---
+++ assets/js/script.js
@@ -0,0 +1,17 @@
+//display current date
+
+
+//color-code blocks with past, present, and future classes
+
+
+
+
+//Save button saves entry to local storage
+
+
+//Text stays on refresh
+
+
+//Update README and save
+//Check no errors in console, push final changes, deploy website
+//submit two links for review
