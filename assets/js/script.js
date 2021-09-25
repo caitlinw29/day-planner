fc84fee322fa6b55d729136639973ace9c0f92bd
Caitlin Wall <caitlinwall96@gmail.com>
2021-09-24 21:42:34 -0400

    Add comments to javascript to explain functionality

--- assets/js/script.js
+++ assets/js/script.js
@@ -3,16 +3,20 @@ var today = moment();
 $("#currentDay").text(today.format("dddd, MMMM Do"));
 
 //color-code blocks with past, present, and future classes
+//pull hour in 24H format to hour variable
 var hour = parseInt(today.format("H"));
 
+//add class of past if the hour assigned to the row is less than the current time
 $("[data-hour]").filter(function() {
     return $(this).data("hour") < hour;
 }).addClass("past");
 
+//add class of present if the hour assigned to the row is the same as the current time
 $("[data-hour]").filter(function() {
     return $(this).data("hour") == hour;
 }).addClass("present");
 
+//add class of future if the hour assigned to the row is greater than the current time
 $("[data-hour]").filter(function() {
     return $(this).data("hour") > hour;
 }).addClass("future");
